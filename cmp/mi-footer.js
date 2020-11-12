54c3ba520322dadc2cb4fba33b370de0c0d4d597
van142415 <74314753+van142415@users.noreply.github.com>
2020-11-12 13:57:10 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; Gilberto Pacheco Gallegos`;
+      `Copyright &copy; Elda Vanessa Vazquez Cisneros`;
   }
 }
 customElements.define("mi-footer", MiFooter);
